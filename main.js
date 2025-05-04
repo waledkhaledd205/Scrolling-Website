4b8df8ef5bd5cbba1fc68f880c288d0a11523b24
unknown <waledkhaledd205@gmail.com>
2025-05-04 04:48:25 +0300

    first

---
+++ main.js
@@ -0,0 +1,37 @@
+let stars = document.getElementById ('stars')
+let moon = document.getElementById ('moon')
+let mountains3 = document.getElementById('mountains3')
+let mountains4 = document.getElementById('mountains4')
+let river = document.getElementById ('river')
+let boat = document.getElementById ('boat')
+let nouvil = document.querySelector('.nouvil') 
+
+window.onscroll = function () {
+    let value = scrollY;
+    stars.style.left = value +'px'
+    moon.style.top = value *4 +'px'
+    mountains3.style.top = value *2 +'px'
+    mountains4.style.top = value *1.5 +'px'
+    river.style.top = value +'px'
+    boat.style.top = value +'px'
+    boat.style.left = value *3 +'px'
+    nouvil.style.fontSize = value  +'px'
+    if (scrollY >= 90) {
+        nouvil.style.fontSize = 90 + 'px'
+        nouvil.style.position = ' fixed';
+    }
+    if (scrollY >= 750) {
+        nouvil.style.display = 'none';
+    } else { 
+        nouvil.style.display = 'block';
+    }
+    if (scrollY >= 257) {
+        document.querySelector('.main').style.background =' linear-gradient( #376281, #10001f)'
+    } else {
+        if (scrollY <= 257) {
+            document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001f)'
+        }
+       
+    }
+
+}
